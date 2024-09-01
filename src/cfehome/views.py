589b3ad3d76de2c6ecb3b7961b32693573d539cb
AlcidesC.Chitsumba <alcidesdaconceicao03@gmail.com>
2024-09-01 10:33:36 +0200

    Hello World

---
+++ src/cfehome/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from  django.http import HttpResponse 
+
+from visits.models import PageVisits
+
+def home(request, *args, **kwargs):
+    qs = PageVisits.objects.all()
+    page_qs =PageVisits.objects.filter(path=request.path)
+    vars = {
+        'name': 'Alcides',
+        'page_visits': page_qs.count(),
+        'total_visits_count': qs.count(),
+    }
+    path = request.path
+    PageVisits.objects.create()
+    return render(request, 'home.html', vars)
+
