7ddcdd7f91de9f0d7c975008a94c3d20fcf5da8d
AlcidesC.Chitsumba <alcidesdaconceicao03@gmail.com>
2024-09-03 22:15:02 +0200

    Setting the DEBUG env var

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kh6ri7r*g%)^_++4apbrv^bpc*838elw2esvtw4&m5d88cnzn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower == 'true'
+print(DEBUG)
 
 ALLOWED_HOSTS = [".railway.app"]
 if DEBUG:
