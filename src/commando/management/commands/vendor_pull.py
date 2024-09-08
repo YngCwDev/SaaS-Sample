993d7c5b541b7acada5741c1a2a8a7e817ef3da5
AlcidesC.Chitsumba <alcidesdaconceicao03@gmail.com>
2024-09-08 18:21:50 +0200

    Dockerfile update

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -5,11 +5,11 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 
-STATICFILES_VENDOR_DIR = getattr(settings,'STATICFILES_VENDOR_DIRS')
+STATICFILES_VENDOR_DIRS = getattr(settings,'STATICFILES_VENDOR_DIRS')
 
 VENDOR_STATICFILES = {
     "flowbite.min.css": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
-    "flowbite.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",   
+    "flowbite.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
 }
 
 class Command(BaseCommand):
@@ -18,7 +18,7 @@ class Command(BaseCommand):
         self.stdout.write("Downloading vendor static files")
         completed_urls = []
         for name, url in VENDOR_STATICFILES.items():
-            out_path = STATICFILES_VENDOR_DIR / name
+            out_path = STATICFILES_VENDOR_DIRS / name
             dl_success = helpers.download_to_local(url, out_path)
             if dl_success:
                 completed_urls.append(url)
